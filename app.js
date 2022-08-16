4f0ba9e9aa564461310fd427df0d6f7c35c36a1b
ShinHanna98 <tls1593575@gachon.ac.kr>
2022-08-16 13:19:55 +0900

    sdfa

--- app.js
+++ app.js
@@ -7,5 +7,7 @@ app.use("/static", express.static("static"));
 const port = 8000;
 
 app.get("/", (req, rew) => {
-    
-})
+    res.render("cardnews");
+});
+
+app.listen(port,()=>{console.log("서버연결")});
