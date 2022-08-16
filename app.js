7545792fdfba1e62dfbbd6e6351ff5ee7c3c6e6f
GarlicScent <dongzai@naver.com>
2022-08-16 15:56:20 +0900

    video_carousel: change render page name in app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use("/static", express.static("static"));
 const port = 8000;
 
 app.get("/", (req, res) => {
-  res.render("main_video");
+  res.render("video_carousel");
 });
 
 app.listen(port, () => console.log("✅ server connected to 8000 port."));
