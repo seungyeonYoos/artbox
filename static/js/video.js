c56caaefb4bfd3e1ad6c6626f7d635c01fbd8caa
GarlicScent <dongzai@naver.com>
2022-08-17 17:31:35 +0900

    inquery: C, MOD - create inquery.js, modified navbar.ejs

--- static/js/video.js
+++ static/js/video.js
@@ -1,7 +1,6 @@
 const videos = document.querySelectorAll("video");
 const nexVideoBtn = document.querySelector(".carousel-control-next");
-const carouselDiv = document.querySelector(".carousel");
-
+// const carouselDiv = document.querySelector(".carousel");
 // carouselDiv.carousel = "pause";
 
 //각각의 video에 ended event가 발생하면 다음 캐러셀 버튼이 클릭된다.
